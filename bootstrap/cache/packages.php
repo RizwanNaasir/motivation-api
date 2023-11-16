f9359efabccc86a84eeb81478bffcff7374f95fb
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-11-16 09:40:44 +0500

    Updated Composer Packages

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,6 +6,13 @@
       0 => 'Dedoc\\Scramble\\ScrambleServiceProvider',
     ),
   ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
   'laravel/sanctum' => 
   array (
     'providers' => 
